95ec8ca821aac62d08ad18721a00d533ea748cb3
peterhoejlundandersen <hilsenit@gmail.com>
2018-02-27 13:17:58 +0100

    created an already votes helper to hide vote up, if a user already has voted

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,12 @@ module PagesHelper
   def helper_get_root_url url
     URI.parse(url).host
   end
+
+  def check_if_already_voted_helper post_id, post_type
+    if post_type == 0 # news
+      current_user.votes.detect{|v| v.news_id == post_id }
+    else
+      current_user.votes.detect{|v| v.question_id == post_id }
+    end
+  end
 end
